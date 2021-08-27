db71b48197b234fdf309f2a6e88bf2ef7c16c518
mujuaj <89645963+mujuaj@users.noreply.github.com>
2021-08-27 23:14:42 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -60,7 +60,8 @@ Join on our channel to get movies ✅
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = caption,
+                caption = f" <code> file_name </code> \n <b>Join [MOVIE HUB HD⬛◼◾▪ ](https://t.me/worldmoviesaj) </b> \n <b> [𝘼𝙇𝙇 𝙈𝙊𝙑𝙄𝙀𝙎 𝘼𝙉𝘿 𝙎𝙀𝙍𝙄𝙀𝙎 𝙃𝘿 ](https://t.me/AJmovieLINKS)<b>© \n Powered by [𝚈𝙾𝚄 𝚁𝙴𝚀𝚄𝙴𝚂𝚃 𝚆𝙴 𝙿𝚁𝙾𝚅𝙸𝙳𝙴](https://t.me/AJmovieLINKS) </b> ",
+                parse_mode="html",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
@@ -80,7 +81,7 @@ Join on our channel to get movies ✅
             await bot.send_video(
                 chat_id=update.chat.id,
                 video = file_id,
-                caption = caption,
+                caption = f" <code> file_name </code> \n <b>Join [MOVIE HUB HD⬛◼◾▪ ](https://t.me/worldmoviesaj) </b> \n <b> [𝘼𝙇𝙇 𝙈𝙊𝙑𝙄𝙀𝙎 𝘼𝙉𝘿 𝙎𝙀𝙍𝙄𝙀𝙎 𝙃𝘿 ](https://t.me/AJmovieLINKS)<b> \n © Powered by [𝚈𝙾𝚄 𝚁𝙴𝚀𝚄𝙴𝚂𝚃 𝚆𝙴 𝙿𝚁𝙾𝚅𝙸𝙳𝙴](https://t.me/AJmovieLINKS) </b> ",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
@@ -119,10 +120,10 @@ Join on our channel to get movies ✅
         return
 
     buttons = [[
-        InlineKeyboardButton('MORE MOVIES', url='https://t.me/AJmovieLINKS'),
+        InlineKeyboardButton('MORE MOVIES', url='https://t.me/worldmoviesaj'),
         InlineKeyboardButton('Source Code 🧾', url ='https://t.me/AJmovieLINKS')],                               
      [
-        InlineKeyboardButton('Support 🛠', url='https://t.me/worldmoviesaj')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/AJmovieLINKS')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
