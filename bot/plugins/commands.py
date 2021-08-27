81752c28eb4b062de86abc9fe71de85ab779ae52
mujuaj <89645963+mujuaj@users.noreply.github.com>
2021-08-27 23:43:33 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -51,7 +51,8 @@ Join on our channel to get movies ✅
         if (file_id or file_type) == None:
             return
         
-        caption =  ("<code>" + file_name + "</code>" != ("" or None) else ("<code>" + file_name + "</code>")
+        caption = <code> file_caption <code> if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        
         
         
         
