d453d796703982ef10d54d4af078a7f070946fb4
mujuaj <89645963+mujuaj@users.noreply.github.com>
2021-09-01 20:25:21 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f'''🄼🄾🅅🄸🄴 🄷🅄🄱 🄷🄳⬛️◼️◾️▪️\n𝐅𝐨𝐫:{}\n<b>🎬𝐌𝐨𝐯𝐢𝐞/𝐒𝐞𝐫𝐢𝐞𝐬:-</b><code>{query}</code>\n🌩️𝐓𝐨𝐭𝐚𝐥 𝐑𝐞𝐬𝐮𝐥𝐭𝐬:{len_results}\n©◻⬜@AJmovieLINKS⬛◼''' ­  ­  ­,
+                text=f'''🄼🄾🅅🄸🄴 🄷🅄🄱 🄷🄳⬛️◼️◾️▪️\n𝐅𝐨𝐫:{}\n<b>🎬𝐌𝐨𝐯𝐢𝐞/𝐒𝐞𝐫𝐢𝐞𝐬:-</b><code>{query}</code>\n🌩️𝐓𝐨𝐭𝐚𝐥 𝐑𝐞𝐬𝐮𝐥𝐭𝐬:{len_results}\n©◻⬜@AJmovieLINKS⬛◼''',
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
